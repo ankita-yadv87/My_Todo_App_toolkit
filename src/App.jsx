7f41d7920869112090ad12651bdfcec5a06e3f53
ankita_yadav87 <ankuy086@gmail.com>
2024-01-21 18:17:57 +0530

    added features to todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import AddTodo from './app/components/AddTodo'
+import Todos from './app/components/Todos'
 
 const App = () => {
   return (
     <>
-    <h1>Welcome to Redux toolkit tutorial
-    </h1>
+      <h1 className="mt-4 text-center font-bold items-center px-4 py-2 rounded text-3xl">Welcome to Todo App
+      </h1>
+      <AddTodo/>
+      <Todos/>
     </>
-    
+
   )
 }
 
--- src/features/todo/todoSlice.js
+++
@@ -1,5 +0,0 @@
-import { CreateSlice, nanoid } from "@reduxjs/toolkit";
-
-const initialState = {
-    todos: [{id:1, text:"Hello world"}]
-}
